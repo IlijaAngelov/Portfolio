b2053a83f9932d21e7c734f7cdd84be29b88beaf
Ilija <ilija.angelov@yahoo.com>
2020-07-23 21:40:55 +0200

    style changes

--- index.php
+++ index.php
@@ -1,43 +1,3 @@
-<?php
-error_reporting(E_ALL ^ E_NOTICE);
-if(isset($_POST['submit'])){ 
-    $name = htmlspecialchars($_POST['name']);
-    $email = htmlspecialchars($_POST['email']);
-    $subject = htmlspecialchars($_POST['subject']);
-    $message = htmlspecialchars($_POST['message']);
-
-    // Checking required fields
-    if(!empty($name) && !empty($email) && !empty($message)){ 
-        if(filter_var($email, FILTER_VALIDATE_EMAIL) === false ) {
-            $form_message = 'Please use valid email!';
-            $form_message_class = 'alert-danger';
-        } else {
-            $toEmail = 'ilija.angelov@yahoo.com';
-            $subject = 'Contact with message: ' .$subject;
-            // $body = '<h2>Contact request</h2>
-            //         <h4>Name</h4><p>'.$name.'</p>
-            //         <h4>Email</h4><p>'.$email.'</p>
-            //         <h4>Message</h4><p>'.$message.'</p>';
-            $body = '<p>THIS IS IT!</p>';
-            $headers = "MIME-Version: 1.0" ."\r\n";
-            $headers.= "Content-Type:text/html;charset=UTF-8" . "\r\n";
-            $headers.= "From: " .$name. "<".$email.">". "\r\n";
-            
-            if(mail($toEmail, $subject, $body, $headers)){
-                $form_message = 'Your email has been sent!';
-                $form_message_class = 'alert-success';
-            } else {
-                $form_message = 'Your email was not sent!';
-                $form_message_class = 'alert-danger';
-            }
-        }
-    } else {
-        $form_message = 'Please Fill all the required Fields!';
-        $form_message_class = 'alert-danger';
-    }
-
-}
-?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -46,9 +6,33 @@ if(isset($_POST['submit'])){
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>My Portfolio</title>
     <link rel="stylesheet" href="style.css" type="text/css">
+    <link rel="stylesheet" href="menu.css" type="text/css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.css"> 
     <script src="https://kit.fontawesome.com/8b76652210.js" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.5.1.js" integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc="
+  crossorigin="anonymous"></script>   
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
 </head>
 <body>
+<?php include ('sendMail.php'); ?>
+<div class="menu-wrap">
+    <input type="checkbox" class="toggler">
+    <div class="hamburger">
+        <div></div>
+    </div>
+    <div class="menu">
+        <div>
+            <div>
+                <ul>
+                    <li><a href="">Home</a></li>
+                    <li><a href="#about">About</a></li>
+                    <li><a href="#projects">Projects</a></li>
+                    <li><a href="#contact-section">Contact</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
 <header>
     <div id="nav" class="menu-area ul">
         <a class="li" href="http://ilija.atrajkov.com/index.php">
@@ -75,35 +59,33 @@ if(isset($_POST['submit'])){
         <h2>Ilija Angelov</h2>
     </div>
     <div class="more-about-myself">
-        I'm a web developer with little more than 2 years of experience in business integration working at StadiaConnect.
-            Working proficiency with html5, css3, javascript, jquery, php, mysql, wordpress.
+        I'm a web developer with little more than 2 years of working experience with business integration at StadiaConnect.
+            Proficiency with html5, css3, javascript, jquery, php, mysql, wordpress.
             I strive to improve my full-stack skills with ES6, Laravel, OOP... 
     </div>
 </div>
-
-<!-- Try scrollable -->
 <div id="projects" class="projects">
 <h1 style="text-align: center;">Projects</h1>
     <div class="projects-container">
         <div class="project">
-            <div class="project-image"><img src="/assets/tree.jpeg" style="width: 100%;"></div>
+            <div class="project-image"><img src="/assets/Basic Tetris.png" style="width: 100%; height: 100%;"></div>
             <div class="project-links">
-                <a href="github.com" >Github</a>
-                <a href="google.com" target="_blank">Live</a>
+                <a href="https://github.com/IlijaAngelov/memory_game" rel="noreferrer">Github</a>
+                <a href="https://codepen.io/ItoAngel/full/jOWpPyx" target="_blank" rel="noreferrer">Live</a>
+            </div>
+        </div>
+        <div class="project">
+            <div class="project-image"><img src="/assets/Memory Game.png" style="width: 100%; height: 100%;"></div>
+            <div class="project-links">
+                <a href="https://github.com/IlijaAngelov/basic-tetris" target="_blank" rel="noreferrer">Github</a>
+                <a href="https://codepen.io/ItoAngel/full/GRoMggB" target="_blank" rel="noreferrer">Live</a>
             </div>
         </div>
         <div class="project">
             <div class="project-image"><img src="/assets/tree.jpeg" style="width: 100%;"></div>
             <div class="project-links">
-                <a href="github.com" target="_blank">Github</a>
-                <a href="google.com" target="_blank">Live</a>
-            </div>
-        </div>
-        <div class="project">
-            <div class="project-image"><img src="/assets/tree.jpeg" style="width: 100%;"></div>
-            <div class="project-links">
-                <a href="github.com" target="_blank">Github</a>
-                <a href="google.com" target="_blank">Live</a>
+                <a href="github.com" target="_blank" rel="noreferrer">Github</a>
+                <a href="google.com" target="_blank" rel="noreferrer">Live</a>
             </div>
         </div>
     </div>
@@ -112,9 +94,6 @@ if(isset($_POST['submit'])){
 <div id="contact-section" class="contact-section">
 <h1 style="text-align: center;" >Contact</h1>
 
-<?php if($form_message != ''): ?>
-    <div class="<?php echo $form_message_class; ?>"><?php echo $form_message; ?></div>
-<?php endif; ?>
     <form id="form" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST" >
         <div class="contact-container">    
             <div class="submit-info">
@@ -132,13 +111,12 @@ if(isset($_POST['submit'])){
     </form>
 </div>
 <footer class="footer" style="text-align: center">
-    <div>selfmade &copy; 2020</divp>
+    <div style="margin-bottom: 5px; ">Copyright &copy; 2020</div>
     <div class="links">
-    <a href="https://www.linkedin.com/in/ilija-angelov-22472413b/" target="_blank"><i class="fab fa-linkedin-in space-it" style="color:white" ></i></a>  
-    <a href="https://github.com/IlijaAngelov" target="_blank"><i class="fab fa-github space-it" style="color:white"></i></a>
-    <a href="http://ilija.atrajkov.com/resume.pdf" target="_blank"><i class="far fa-file-alt space-it" style="color:white"></i></a>
+    <a href="https://www.linkedin.com/in/ilija-angelov-22472413b/" target="_blank" rel="noreferrer"><i class="fab fa-linkedin-in space-it" style="color:white" ></i></a>  
+    <a href="https://github.com/IlijaAngelov" target="_blank" rel="noreferrer"><i class="fab fa-github space-it" style="color:white"></i></a>
+    <a href="http://ilija.atrajkov.com/resume.pdf" target="_blank" rel="noreferrer"><i class="far fa-file-alt space-it" style="color:white"></i></a>
     </div>
-
 </footer>
 <script type="text/javascript" src="scripts.js"></script>
 </body>
